5ad87bf781b7a9d3538e3da8bc7a9438426113e5
Daniel Subauste Oliden <daalsuol@gmail.com>
2015-02-04 20:05:51 +0000

    chat

---
+++ UPCchatTest/app/models/message.rb
@@ -0,0 +1,12 @@
+class Message < ActiveRecord::Base
+  Pusher.app_id = 105925
+  Pusher.key = 'c3d762824583fa9cc453'
+  Pusher.secret = '33239ee0daf01a08f32e'
+
+  after_create :send_to_pusher
+
+  def send_to_pusher
+  	logger.info "Hi?"
+  	Pusher['DEW2015'].trigger("message:create", self.to_json)
+  end
+end
